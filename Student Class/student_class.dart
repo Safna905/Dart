2746bdeb9678128555e5e1710e63294f7feb0204
Safna905 <eksafna786@gmail.com>
2023-03-04 17:03:32 +0530

    update 1

---
+++ Student Class/student_class.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+class Student {
+  String name;
+  int roll_no;
+
+  Student(this.name, this.roll_no);
+}
+
+void main() {
+  Student stud = Student('John', 2);
+  print('Name : ${stud.name}\nRoll No : ${stud.roll_no}');
+}
